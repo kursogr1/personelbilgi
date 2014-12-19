4c3e5acdd5aa0fa19686c24123314aef5d0efa15
SBN <SBN@192.168.2.36>
2014-12-19 13:56:25 +0200

    kayit bean ekle

--- src/main/java/com/javakurs/personelbilgi/bean/KayitBean.java
+++ src/main/java/com/javakurs/personelbilgi/bean/KayitBean.java
@@ -7,6 +7,10 @@ package com.javakurs.personelbilgi.bean;
 
 import com.javakurs.personelbilgi.entity.Kisi;
 import com.javakurs.personelbilgi.entity.Telefon;
+import com.javakurs.personelbilgi.service.KisiService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
@@ -17,35 +21,49 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "kayitBean")
 @RequestScoped
 public class KayitBean {
+    @EJB
+    private KisiService kisiService;
+    // dependence advance
+    private Kisi kisi = new Kisi();
+    private Telefon cepTel = new Telefon();
+    private Telefon evTel = new Telefon();
 
-   private Kisi kisi = new Kisi();
-   private Telefon cepTel= new Telefon();
-   private Telefon evTel = new Telefon();
     public KayitBean() {
     }
-
+    
     public Kisi getKisi() {
         return kisi;
     }
-
+    
     public void setKisi(Kisi kisi) {
         this.kisi = kisi;
     }
-
+    
     public Telefon getCepTel() {
         return cepTel;
     }
-
+    
     public void setCepTel(Telefon cepTel) {
         this.cepTel = cepTel;
     }
-
+    
     public Telefon getEvTel() {
         return evTel;
     }
-
+    
     public void setEvTel(Telefon evTel) {
         this.evTel = evTel;
     }
-    
+
+    public void ekle() {
+
+//    one to many cift tarflı set ikisinden de set etmek gerekyor
+        List<Telefon> telefonList = new ArrayList<>();
+        telefonList.add(evTel);
+        telefonList.add(cepTel);
+        kisi.setTelefonList(telefonList);
+        cepTel.setKisi(kisi);
+        evTel.setKisi(kisi);
+        kisiService.ekle(kisi);
+    }
 }
